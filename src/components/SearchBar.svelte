ae9cfef9ed52640b66118f4e62dd4ff821df9309
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-03-12 22:54:23 +0530

    🚀 Added redirection for input query

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,5 +1,12 @@
 <script>
-
+  window.onload=function(){
+      const form = document.querySelector('form');
+      form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const query = form.querySelector('input').value;
+        window.location.href = `https://www.google.com/search?q=${query}`;
+      });
+  }
 </script>
 
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
@@ -7,13 +14,14 @@
 <div class="searchBody">
   <div class="wrapper">
     <div class="label">Google Search</div>
-    <div class="searchBar">
+    <form class="searchBar">
       <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Search" value="" />
-      <button id="searchQuerySubmit" name="searchQuerySubmit">
-        <svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+      <button id="searchQuerySubmit" name="searchQuerySubmit" type="">
+        <svg style="width:24px; height:24px" viewBox="0 0 24 24">
+          <path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
         </svg>
       </button>
-    </div>
+    </form>
   </div>
 </div>
 
@@ -29,7 +37,7 @@
     }
 
     .label {
-      font-size: .625rem;
+      font-size: 1rem;
       font-weight: 400;
       text-transform: uppercase;
       letter-spacing: +1.3px;
@@ -67,4 +75,5 @@
     #searchQuerySubmit:hover {
       cursor: pointer;
     }
+
 </style>
