4fd3c3855abc44342cb665e92793379b1a621ab6
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-03-23 13:53:13 +0530

    ✨ Changed class names

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,6 +1,6 @@
 <script>
   window.onload=function(){
-      const form = document.querySelector('.searchBar');
+      const form = document.querySelector('.search-bar');
       form.addEventListener('submit', function(e) {
         e.preventDefault();
         const query = form.querySelector('input').value;
@@ -11,12 +11,12 @@
 
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
-<div class="searchBody">
+<div class="search-body">
   <div class="wrapper">
     <div class="label">Google Search</div>
-    <form class="searchBar">
-      <input id="searchQueryInput" type="text" onfocus="this.value=''" name="searchQueryInput" placeholder="Search" value="" />
-      <button id="searchQuerySubmit" name="searchQuerySubmit" type="">
+    <form class="search-bar">
+      <input id="search-query-input" type="text" onfocus="this.value=''" name="search-query-input" placeholder="Search" value="" />
+      <button id="search-query-submit" name="search-query-submit" type="">
         <svg style="width:24px; height:24px" viewBox="0 0 24 24">
           <path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
         </svg>
@@ -26,7 +26,7 @@
 </div>
 
 <style>
-    .searchBody {
+    .search-body {
       font-family: 'Roboto', Arial, sans-serif;
     }
 
@@ -44,14 +44,14 @@
       margin-bottom: 1rem;
     }
 
-    .searchBar {
+    .search-bar {
       width: 100%;
       display: flex;
       flex-direction: row;
       align-items: center;
     }
 
-    #searchQueryInput {
+    #search-query-input {
       width: 100%;
       height: 2.8rem;
       background: #f5f5f5;
@@ -63,7 +63,7 @@
       border:1px solid black
     }
 
-    #searchQuerySubmit {
+    #search-query-submit {
       width: 3.5rem;
       height: 2.8rem;
       margin-left: -3.5rem;
@@ -72,7 +72,7 @@
       outline: none;
     }
 
-    #searchQuerySubmit:hover {
+    #search-query-submit:hover {
       cursor: pointer;
     }
 
