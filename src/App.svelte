5559c14029bee41b54c2ce5dd30499213c701a86
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-04-02 19:13:21 +0530

    💄 Added NavBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,28 @@
 <script>
 	import SearchBar from "./components/SearchBar.svelte";
+	import StackOverflowSearchBar from "./components/StackOverflowSearchBar.svelte";
+	import Socials from "./components/Socials.svelte";
 	import Footer from "./components/Footer.svelte";
-	let name = "Starpage";
+	import NavBar from "./components/NavBar.svelte";
+	import ToDoList from "./components/ToDoList.svelte";
+	let name = "Startpage";
 	let version = "0.0.1";
-
+	let activeTab = 'Home';
+	
 </script>
 
 <main>
+	<NavBar bind:activeTab={activeTab} />
 	<h1>{name}</h1>
-	<SearchBar />
+	{#if activeTab == 'Home'}
+		<SearchBar />
+	{:else if activeTab == 'Profile'}
+		<Socials />
+	{:else if activeTab == 'Developer'}
+		<StackOverflowSearchBar />
+	{:else if activeTab == 'To-Do'}
+		<ToDoList />
+	{/if}
 </main>
 <Footer {name} {version} />
 
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,65 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    export let activeTab = '';
+
+    const tabs = [
+        { name: 'Home', icon: '🏠' },
+        { name: 'Profile', icon: '👤' },
+        { name: 'Developer', icon: '💬' },
+        { name: 'To-Do', icon: '📝' }
+    ];
+
+    function selectTab(tab) {
+        activeTab = tab.name;
+        dispatch('tabSelected', activeTab);
+    }
+
+</script>
+  
+<nav>
+  {#each tabs as tab}
+    <button
+      class={`nav-button ${activeTab === tab.name ? 'active' : ''}`}
+      on:click={() => selectTab(tab)}
+    >
+      <span class="nav-icon">{tab.icon}</span>
+    </button>
+  {/each}
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+    }
+
+    .nav-button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        width: 3rem;
+        height: 3rem;
+        border-radius: 50%;
+        border: none;
+        background-color: #eee;
+        color: #333;
+        font-size: 1.5rem;
+        cursor: pointer;
+        transition: all 0.2s;
+    }
+
+    .nav-button.active {
+        background-color: #333;
+        color: #eee;
+    }
+
+    .nav-icon {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+</style>
