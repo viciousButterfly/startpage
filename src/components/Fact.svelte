84bb795400021731607a2770dfcc08f7494da5c7
vaishnavisp13 <vaishnavisp13@gmail.com>
2023-04-04 10:27:27 +0530

    ✨ Added random facts

---
+++ src/components/Fact.svelte
@@ -0,0 +1,14 @@
+<script>
+  let devFact = "";
+
+  const fetchDevFact = async () => {
+    const response = await fetch('https://uselessfacts.jsph.pl/random.json?category=programming');
+    const data = await response.json();
+    devFact = data.text;
+  };
+</script>
+
+<button on:click={fetchDevFact}>Get a random developer fact</button>
+{#if devFact}
+  <p>{devFact}</p>
+{/if}
